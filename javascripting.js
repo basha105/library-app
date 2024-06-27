64f4593a7562e6a210aa7fb90840cffdf64ea64a
basha105 <brentbasha17@gmail.com>
2024-06-27 13:52:21 -0700

    Completed displayLibrary func

--- javascripting.js
+++ javascripting.js
@@ -5,3 +5,34 @@ function Book(title, author, year) {
     this.author = author;
     this.year = year;
 }
+
+function addBookToLibrary(myBook) {
+    myLibrary.push(myBook);
+}
+
+const container = document.querySelector("#container");
+
+function displayLibrary(currentLibrary) {
+    for (let i=0; i<currentLibrary.length; i++) {
+        let thisBook = currentLibrary[i];
+        const currentCard = document.createElement("div");
+        currentCard.classList.add("cards");
+        currentCard.textContent = String(thisBook.title) + " by " + String(thisBook.author) + ", published in " + String(thisBook.year);
+        container.appendChild(currentCard);
+    }
+ }
+
+const newBookButton = document.querySelector("#newBook");
+newBookButton.addEventListener("click", () => {
+    
+})
+
+
+
+ // let newBook = new Book("Gatsby", "Fitzgerald", 1925);
+ // addBookToLibrary(newBook);
+ // displayLibrary(myLibrary);
+
+
+
+
